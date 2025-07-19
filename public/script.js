ac884d1321cb48d7526f5817cd916b88724f4eaf
celake <me@charlottelake.eu>
2025-07-19 16:35:52 +0100

    feat: add filtering based on group dropdown selection in contact list

--- public/script.js
+++ public/script.js
@@ -13,3 +13,40 @@ tags.forEach(tag => {
         tag.classList.toggle('checked', checkbox.checked);
     })
 })
+
+
+// open dropdown menu on main page
+
+const toggleOn = document.querySelector('.toggleOn');
+const selected = document.querySelector('.dropdown-selected');
+const options = document.querySelector('.dropdown-options')
+
+toggleOn.addEventListener('click', () => {
+    selected.classList.add('hidden');
+    options.classList.remove('hidden');
+})
+
+options.addEventListener('click', (evt) => {
+    const selectedGroup = evt.target;
+    const groupName = selectedGroup.textContent;
+    const groupId = selectedGroup.dataset.value;
+    const selectedText = selected.querySelector('span')
+    selectedText.innerText = groupName;
+    selected.dataset.value = groupId;
+    selected.classList.remove('hidden');
+    options.classList.add('hidden');
+    
+    // filter list
+    const contacts = document.querySelectorAll('.contact-item');
+    contacts.forEach(contact => {
+        const contactGroups = contact.dataset.groups.split(',');
+        console.log(contact, contactGroups.includes(groupId));
+        if (groupId === "all" || contactGroups.includes(groupId)) {
+            contact.classList.remove('hidden');
+        } else {
+            contact.classList.add('hidden');
+        }
+    })
+})
+
+
